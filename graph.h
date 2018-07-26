f9bb71c92fe75390f2b88aabad6bb9e80d374b22
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-07-26 11:38:32 -0700

    Include printGraphNodes and printGraphArcs functions for Graph data structure.

--- graph.h
+++ graph.h
@@ -54,6 +54,76 @@ void addArc(Graph & g, Node *startNode, Node *finishNode, char direction, float 
       finishNode->arcs.add(arc); // this would have to be a reversed arc
 }
 
+void printGraphNodes(Graph & g) {
+    Vector<Node *> graphNodes = g.nodes;
+    Serial.println("***************************");
+    Serial.print("Printing ");
+    Serial.print(graphNodes.size());
+    Serial.println(" nodes for newGraph.");
+    
+    for(int i = 0; i < graphNodes.size(); i++) {
+        Node *currentNode = graphNodes.get(i);
+        Vector<float> distancesToWalls = currentNode->distancesToWalls;
+        Vector<char> currentOpenings = currentNode->openings;
+        
+        Serial.print("Node: ");
+        Serial.println(currentNode->order);
+        
+        Serial.print("\tNode Distances to Walls: ");
+        Serial.print("F: ");
+        Serial.print(distancesToWalls[0]);
+        Serial.print(", L: ");
+        Serial.print(distancesToWalls[1]);
+        Serial.print(", R: ");
+        Serial.println(distancesToWalls[2]);
+        
+        Serial.print("\tNode openings: ");
+        for(int j = 0; j < currentOpenings.size(); j++) {
+            if (j > 0) { Serial.print(", "); }
+            Serial.print(currentOpenings[j]);
+        }
+        Serial.println("");
+        
+        Serial.print("\tDistance Travelled: ");
+        Serial.println(currentNode->distanceTravelled);
+        
+        Serial.print("\tVisited: ");
+        Serial.println(currentNode->visited);
+    }
+    
+    Serial.println("***************************");
+    
+}
+
+void printGraphArcs(Graph & g) {
+    Vector<Arc *> graphArcs = g.arcs;
+    Serial.println("***************************");
+    Serial.print("Printing ");
+    Serial.print(graphArcs.size());
+    Serial.println(" arcs for newGraph.");
+    
+    for(int i = 0; i < graphArcs.size(); i++) {
+        Arc *currentArc = graphArcs.get(i);
+        
+        Serial.print("Arc ");
+        Serial.println(i + 1);
+        
+        Serial.print("\tStarting Node: ");
+        Serial.println(currentArc->start->order);
+        
+        Serial.print("\tEnding Node: ");
+        Serial.println(currentArc->finish->order);
+        
+        Serial.print("\tDirection from start to finish: ");
+        Serial.println(currentArc->direction);
+        
+        Serial.print("\tDistance between start and finish: ");
+        Serial.println(currentArc->distanceBetweenNodes);
+    }
+    
+    Serial.println("***************************");
+}
+
 #endif 
 
 
