bd4c01e50119535c6b8170aa9819c716ee85e507
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-07-26 11:37:25 -0700

    Include addNode and addArc functions for Graph data structure.

--- graph.h
+++ graph.h
@@ -32,6 +32,28 @@ struct Arc {
   float distanceBetweenNodes;
 };
 
+void addNode(Graph & g, int order, Vector<float> distancesToWalls, Vector<char> openings, float distanceTravelled) {
+    Node *node = new Node;
+    node->order = order;
+    node->distancesToWalls = distancesToWalls;
+    node->openings = openings;
+    node->distanceTravelled = distanceTravelled;
+    node->visited = true;
+    g.nodes.add(node);
+}
+
+void addArc(Graph & g, Node *startNode, Node *finishNode, char direction, float distanceBetweenNodes) {
+    Arc *arc = new Arc;
+    arc->start = startNode;
+    arc->finish = finishNode;
+    arc->direction = direction;
+    arc->distanceBetweenNodes = distanceBetweenNodes;
+    g.arcs.add(arc);
+    // do I need to add this?
+      startNode->arcs.add(arc);
+      finishNode->arcs.add(arc); // this would have to be a reversed arc
+}
+
 #endif 
 
 
